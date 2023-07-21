a920b86d5f073806f528d2b013c0ea3d263ddec4
FredB83 <106035720+FredB83@users.noreply.github.com>
2023-07-21 22:00:29 +0200

    Update Snake.py

--- Snake.py
+++ Snake.py
@@ -1,1 +1,41 @@
+import pygame
 
+# Example file showing a basic pygame "game loop"
+import pygame
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+color=(255,0,0)
+flag= False
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    
+    for event in pygame.event.get():
+        screen.fill("blue")
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_c:
+                flag= not flag
+                
+    if flag:
+        pygame.draw.rect(screen, color, (30, 30, 60, 60))
+                
+
+
+
+    # fill the screen with a color to wipe away anything from last frame
+
+    # RENDER YOUR GAME HERE
+
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    clock.tick(60)  # limits FPS to 60
+
+pygame.quit()
